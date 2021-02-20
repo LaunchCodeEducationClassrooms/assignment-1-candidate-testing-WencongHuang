1a12f6c2d54a307293de8f3e11861542f67447f4
Edmond Huang <vincent20100517@hotmail.com>
2021-02-20 04:15:37 +0000

    Use single quotation instead of double for questions Array and correct Array.

--- candidate-testing.js
+++ candidate-testing.js
@@ -14,12 +14,12 @@ let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
 
-let questions = ["Who was the first American woman in space? ",
-"True or false: 5000 meters = 5 kilimeters. ",
-"(5 + 3)/2 * 10 = ? ",
-"Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
-"What is the minimum crew size for the ISS? "];
-let correctAnswers = ["Sally Ride", "True", "40", "Trajectory", "3"];
+let questions = ['Who was the first American woman in space? ',
+'True or false: 5000 meters = 5 kilimeters. ',
+'(5 + 3)/2 * 10 = ? ',
+'Given the array [8, \'Orbit\', \'Trajectory\', 45], what entry is at index 2? ',
+'What is the minimum crew size for the ISS? '];
+let correctAnswers = ['Sally Ride', 'True', '40', 'Trajectory', '3'];
 let candidateAnswers = [];
 
 function askForName() {
